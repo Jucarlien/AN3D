b26812f2a4dc5714c7c7372953fd5157a53ff840
Julien <julien.cardon@epita.fr>
2021-11-14 17:08:23 -0800

    rotation camera

--- scenes/animation/02_simulation/sphere_collision.cpp
+++ scenes/animation/02_simulation/sphere_collision.cpp
@@ -18,15 +18,15 @@ void scene_model::frame_draw(std::map<std::string,GLuint>& , scene_structure& sc
     set_gui();
 
     create_new_particle();
-    compute_time_step(dt);
+    compute_time_step(dt, scene);
 
     display_particles(scene);
     draw(borders, scene.camera);
-
 }
 
-void scene_model::compute_time_step(float dt)
+void scene_model::compute_time_step(float dt, scene_structure& scene)
 {
+    float epsilon = 0.01;
     // Set forces
     const size_t N = particles.size();
 
@@ -45,14 +45,7 @@ void scene_model::compute_time_step(float dt)
         float friction = 0.7;
 
 
-        //GRAVITY
-        sphere.f = vec3(0,-9.81f,0);
 
-        vec3 const& f = sphere.f;
-
-        v1 = (1-0.9*dt) * v1 + dt * f; // gravity + friction force
-        //v1 *= friction;
-        p1 = p1 + dt * v1;
    
 
 
@@ -65,15 +58,6 @@ void scene_model::compute_time_step(float dt)
             auto r2 = sphere2.r;
             auto p2 = sphere2.p;
 
-            
-            //std::cout << "test" << "\n";
-            if (norm(v2) + norm(v1) < 0.000001){
-                  //std::cout << norm(v2) + norm(v1) << "\n";
-                  //std::cout.flush();
-                  v2 *= 0.7;
-                  v1 *= 0.7;
-            }
-
             //auto pnorm = std::sqrt(dot(p1 - p2, p1 - p2));
             auto u = (p1 - p2) / norm(p1 - p2);
             auto v1n = dot(v1, u) * u;
@@ -85,17 +69,25 @@ void scene_model::compute_time_step(float dt)
             {
                 //v1 = 0.9 * v1t + 0.9 * v2n;
                 //v2 = 0.9 * v2t + 0.9 * v1n;
+                if (norm(v2) + norm(v1) < 0.1) {
+                    //v2 *= friction;
+                    v1 *= friction;
+                    v2 *= friction;
+                    continue;
+                }
                 v1 += friction * (dot(v2 - v1, u) * u);
                 v2 -= friction * (dot(v2 - v1, u) * u);
                 float d = r1 + r2 - norm(p1 - p2);
-                sphere.p += d * u;
+                sphere.p += (d / 2) *  u;
+                //sphere.p += d * u;
             }
-
         }
 
 
         if (norm(v1) < 0.0001)
+        {
             friction = 0.1;
+        }
         //BORDERS COLLISION
         for(size_t i = 0; i < 6; i++)
         {
@@ -111,11 +103,89 @@ void scene_model::compute_time_step(float dt)
                 //v = -vn * n + vt;
             } 
         }
+
+        //GRAVITY
+        sphere.f = vec3(0,-9.81f,0);
+        sphere.f = update_gravity(scene);
+        vec3 const& f = sphere.f;
+
+        v1 = (1-0.9*dt) * v1 + dt * f; // gravity + friction force
+        //v1 *= friction;
+        p1 = p1 + dt * v1;
+
+
     }
 
     // Collisions between spheres
     // ... to do
 
+    //update_translation(scene);
+}
+
+
+
+float prev_a = 100000000;
+vec3 grav = {0,0,0};
+
+
+vec3 scene_model::update_gravity(scene_structure& scene){
+    auto mat = scene.camera.orientation;
+    float g = -9.81;
+
+    float x = mat[1];
+    float y = mat[4];
+    float z = mat[7];
+    
+    grav = vec3(x * g, y * g, z * g);
+
+    return grav;
+
+}
+
+vec3 scene_model::update_translation(scene_structure& scene){
+    //vec3 trans = scene.camera.translation;
+    //std::cout<< trans << "\n\n";
+}
+
+
+vec3 scene_model::determine_gravity(scene_structure& scene) {
+    float a = gui_scene.gravity;
+    if (a == prev_a){
+
+        return grav;
+    }
+    prev_a = a;
+    float g = -9.81;
+    float y = g * a;
+    
+    float temp = 1 - a;
+    float x = 0;
+    if (a / 90 < -1)
+    {
+         x = (a + 90) / 90;
+        y = -1 - x;
+    }
+
+    else if (a / 90 < 0)
+    {
+        y = a / 90;
+        x = 1 + y;
+    }
+    
+    else if (a / 90 > 1)
+    {
+        x = -(a - 90) / 90;
+        y = 1 + x;
+    }
+
+    else if (a / 90 >= 0)
+    {
+        y = (a / 90);
+        x = 1 - y;
+    }
+    grav = vec3(x * g, 0, y * g);
+    scene.camera.apply_rotation(0.1, 0.0, 0.0, 0);
+    return grav;
 }
 
 
@@ -156,12 +226,13 @@ void scene_model::display_particles(scene_structure& scene)
         sphere.uniform.color = part.c;
         draw(sphere, scene.camera);
     }
+    draw(cube, scene.camera);
 }
 
 
 
 
-void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_structure& , gui_structure& )
+void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& )
 {
     sphere = mesh_drawable( mesh_primitive_sphere(1.0f));
     sphere.shader = shaders["mesh"];
@@ -173,6 +244,10 @@ void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_struct
     borders.uniform.color = {0,0,0};
     borders.shader = shaders["curve"];
 
+    cube = mesh_drawable( mesh_primitive_parallelepiped(vec3(0,0,0),vec3(0.5,0.5,0),vec3(0,0.5,0.5),vec3(0.5,0,0.5)));
+    //cube = mesh_drawable(mesh_primitive_cone(0.1f, {0,0,0,},{0,0.2f,0}));
+    cube.uniform.color = {50,50,50};
+    cube.shader = shaders["mesh"];
 }
 
 
@@ -183,7 +258,9 @@ void scene_model::set_gui()
     ImGui::SliderFloat("Time scale", &timer.scale, 0.05f, 2.0f, "%.2f s");
     ImGui::SliderFloat("Interval create sphere", &gui_scene.time_interval_new_sphere, 0.05f, 2.0f, "%.2f s");
     ImGui::Checkbox("Add sphere", &gui_scene.add_sphere);
-
+//    
+    ImGui::SliderFloat("rotation", &gui_scene.gravity, -180.0f,180.0f, "%.2f G");
+//
     bool stop_anim  = ImGui::Button("Stop"); ImGui::SameLine();
     bool start_anim = ImGui::Button("Start");
 
--- scenes/animation/02_simulation/sphere_collision.hpp
+++ scenes/animation/02_simulation/sphere_collision.hpp
@@ -19,6 +19,7 @@ struct gui_scene_structure
 {
     bool add_sphere = true;
     float time_interval_new_sphere = 0.5f;
+    float gravity = 1.0f;
 };
 
 struct scene_model : scene_base
@@ -29,21 +30,24 @@ struct scene_model : scene_base
 
     void set_gui();
 
-    void compute_time_step(float dt);
+    void compute_time_step(float dt, scene_structure& scene);
     void create_new_particle();
     void display_particles(scene_structure& scene);
+    vcl::vec3 update_gravity(scene_structure& scene);
+    vcl::vec3 determine_gravity(scene_structure& scene);
 
+    vcl::vec3 update_translation(scene_structure& scene);
 
     std::vector<particle_structure> particles;
 
     vcl::mesh_drawable sphere;      // Visual display of particles
     vcl::segments_drawable borders; // Visual display of borders
+    
+    vcl::mesh_drawable cube; //cube display
 
     vcl::timer_event timer;
     gui_scene_structure gui_scene;
 };
-
-
 
 
 
